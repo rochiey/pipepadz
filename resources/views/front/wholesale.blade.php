d373521f441ee01658d4bb0a42883d3aea7d50ca
Rochie <dev@rochieyamit.com>
2020-05-11 05:58:48 +0800

    added wholesale page

---
+++ resources/views/front/wholesale.blade.php
@@ -0,0 +1,251 @@
+@extends('front.index')
+
+@section('content')
+<section id="main" role="main" class="clearfix">
+  <div class="margin">
+  
+    <div class="content-wrap clearfix">
+  
+	<div id="content" class="clearfix">
+		  
+	  <article id="post-2708" class="post-2708 page type-page status-publish hentry">
+		<div class="entry">
+
+		  <div class="postcontent">
+			<p style="text-align: center;"></p><div class="ps-column"><div class="twocol-one"><strong><span style="font-size: 18px;">Local 321·747·7102<br>
+Toll‌ Free‌ (888)‌·579·4543</span></strong><br>
+<b>Hours: Mon-Fri: 9-5 EST</b><br>
+<a href="mailto:info@pipepadz.com">info@pipepadz.com</a></div>
+<div class="twocol-one last">
+‌PipePadz<br>
+‌PO BOX 561002<br>
+‌Rockledge,‌ Florida‌ 32955</div>
+<div class="ps-divider flat"></div></div><p></p>
+<div>If you’re a wholesaler or retailer, please contact us via the form below to get more information on our wholesale program. A representative will be in touch with you within 24-48 hours.</div>
+<p>&nbsp;</p>
+<div>
+
+<!-- Fast Secure Contact Form plugin 4.0.55 - begin - FastSecureContactForm.com -->
+<div style="clear:both;"></div>
+
+<div id="FSContact1" style="width:99%; max-width:555px;">
+<form action="https://pipepadz.com/wholesale/#FSContact1" id="fscf_form1" method="post">
+
+<div id="fscf_div_clear1_0" style="clear:both;">
+  <div id="fscf_div_field1_0" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+
+    <div style="clear:both;"></div>
+<div id="wholesale_left">
+<strong>Store Info</strong>
+
+    <div id="fscf_label1_0" style="text-align:left; padding-top:5px;">
+    </div>
+	<div style="text-align:left; padding-top:5px;">
+    </div>
+	<div style="text-align:left;">
+	  <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_name1" name="full_name" value="" placeholder="*Name">
+	</div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_1" style="clear:both;">
+  <div id="fscf_div_field1_1" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_1" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_email1" name="email" value="" placeholder="*Email">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_4" style="clear:both;">
+  <div id="fscf_div_field1_4" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_4" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_4" name="website-store" value="" placeholder="*Website / Store">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_5" style="clear:both;">
+  <div id="fscf_div_field1_5" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_5" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_5" name="tax-id" value="" placeholder="Tax ID">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_6" style="clear:both;">
+  <div id="fscf_div_field1_6" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_6" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_6" name="phone-number" value="" placeholder="*Phone Number">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_12" style="clear:both;">
+  <div id="fscf_div_field1_12" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_12" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_12" name="how-did-your-hear-about-us" value="" placeholder="How Did Your Hear About Us?">
+    </div>
+
+    <div style="clear:both;"></div>
+</div>
+
+  </div>
+</div>
+
+<div id="fscf_div_clear1_7" style="clear:both;">
+  <div id="fscf_div_field1_7" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+
+    <div style="clear:both;"></div>
+<div id="wholesale_right">
+<strong>Address</strong><br>
+
+    <div id="fscf_label1_7" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_7" name="street-address" value="" placeholder="*Address">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_8" style="clear:both;">
+  <div id="fscf_div_field1_8" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_8" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_8" name="address-line-2" value="" placeholder="Address Line 2">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_9" style="clear:both;">
+  <div id="fscf_div_field1_9" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_9" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_9" name="city" value="" placeholder="*City">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_10" style="clear:both;">
+  <div id="fscf_div_field1_10" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_10" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_10" name="state-province-region" value="" placeholder="*State / Province / Region">
+    </div>
+  </div>
+</div>
+
+<div id="fscf_div_clear1_11" style="clear:both;">
+  <div id="fscf_div_field1_11" style="clear:left; float:left; width:99%; max-width:550px; margin-right:10px;">
+    <div id="fscf_label1_11" style="text-align:left; padding-top:5px;">
+    </div>
+    <div style="text-align:left;">
+      <input style="text-align:left; margin:0; width:99%; max-width:250px;" type="text" id="fscf_field1_11" name="postal-zip-code" value="" placeholder="*Postal / Zip Code">
+    </div>
+
+    <div style="clear:both;"></div>
+</div>
+
+  </div>
+</div>
+<div style="clear:both;"></div>
+
+<div style="text-align:left; padding-top:5px;">
+</div>
+<div id="fscf_captcha_image_div1" style="width:250px; height:65px; padding-top:2px;">
+      <img id="fscf_captcha_image1" style="border-style:none; margin:0; padding:0px; padding-right:5px; float:left;" src="https://pipepadz.com/wp-content/plugins/si-contact-form/captcha/securimage_show.php?prefix=W476cHndzz9mfqNK" width="175" height="60" alt="CAPTCHA" title="CAPTCHA">
+      <input id="fscf_captcha_prefix1" type="hidden" name="fscf_captcha_prefix1" value="W476cHndzz9mfqNK">
+     <div id="fscf_captcha_refresh1">
+        <a href="#" rel="nofollow" title="Refresh" onclick="fscf_captcha_refresh('1','/wp-content/plugins/si-contact-form/captcha','https://pipepadz.com/wp-content/plugins/si-contact-form/captcha/securimage_show.php?prefix='); return false;">
+        <img src="https://pipepadz.com/wp-content/plugins/si-contact-form/captcha/images/refresh.png" width="22" height="20" alt="Refresh" style="border-style:none; margin:0; padding:0px; vertical-align:bottom;" onclick="this.blur();"></a>
+	 </div>
+</div>
+
+<div id="fscf_captcha_field1" style="text-align:left; padding-top:5px;">
+	 <label style="text-align:left;" for="fscf_captcha_code1">CAPTCHA Code:<span style="text-align:left;">*</span></label>
+</div>
+<div style="text-align:left;">
+     <input style="text-align:left; margin:0; width:50px;" type="text" value="" autocomplete="off" id="fscf_captcha_code1" name="captcha_code">
+</div>
+
+<div id="fscf_submit_div1" style="text-align:left; clear:both; padding-top:15px;">
+		<input type="submit" id="fscf_submit1" style="cursor:pointer; margin:0;" value="Submit" onclick="this.disabled=true; this.value='Submitting...'; this.form.submit();"> <input type="reset" id="fscf_reset1" style="cursor:pointer; margin:0;" value="Reset" onclick="return confirm('Do you really want to reset the form?')">
+</div>
+
+<input type="hidden" name="fscf_submitted" value="0">
+<input type="hidden" name="fs_postonce_1" value="8478d9d730322140572290229f895bdc,1589147249">
+<input type="hidden" name="si_contact_action" value="send">
+<input type="hidden" name="form_id" value="1">
+<input type="hidden" name="mailto_id" value="1">
+
+</form>
+</div>
+<div style="clear:both;"></div>
+
+<style type="text/css">
+
+/* Placeholder Style - WebKit browsers - Safari, Chrome */
+::-webkit-input-placeholder { opacity:0.6; color:#333333; }
+
+/* Placeholder Style - Mozilla Firefox 4 - 18 */
+:-moz-placeholder { opacity:0.6; color:#333333; }
+
+/* Placeholder Style - Mozilla Firefox 19+ */
+::-moz-placeholder { opacity:0.6; color:#333333; }
+
+/* Placeholder Style - Internet Explorer 10+ */
+:-ms-input-placeholder { opacity:0.6; color:#333333; }
+
+</style>
+
+<!-- Fast Secure Contact Form plugin 4.0.55 - end - FastSecureContactForm.com -->
+</div>
+								  </div>
+
+		</div>
+	  </article>
+
+	
+		</div>
+	
+	<aside id="sidebar" class="sidebar">
+   
+    		<div id="recent-posts-6" class="widget widget-container widget-sidebar widget_recent_entries">		<h3>Recent Posts</h3>		<ul>
+											<li>
+					<a href="https://pipepadz.com/hempbeach-review/">PIPEPADZ FEATURED ON HEMP BEACH TV</a>
+									</li>
+											<li>
+					<a href="https://pipepadz.com/water-pipe-protection/">THE ULTIMATE WATER PIPE PROTECTION!</a>
+									</li>
+											<li>
+					<a href="https://pipepadz.com/protect/">PROTECTING YOUR GLASS</a>
+									</li>
+											<li>
+					<a href="https://pipepadz.com/washing-your-pipepadz/">WASHING YOUR PIPEPADZ™</a>
+									</li>
+											<li>
+					<a href="https://pipepadz.com/bong-protection/">PIPE COASTERS &amp; BONG PROTECTORS</a>
+									</li>
+					</ul>
+		</div>  </aside>
+	
+	</div>
+	
+		
+  </div>
+</section>
+@endsection
