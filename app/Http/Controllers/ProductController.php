e99d08bd295982238e1e03b3f8b7e1cbf923fc57
Rochie <dev@rochieyamit.com>
2020-05-22 02:22:30 +0800

    added subtotal in session

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -19,66 +19,66 @@ class ProductController extends Controller
 		        case '1':
 		        	if(isset($cart[$_GET['add-to-cart']])){
 		        		$sub_cart_quantity = $cart[$_GET['add-to-cart']]['quantity'];
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Nautical Star', 'price' => '7.99', 'quantity' => $sub_cart_quantity+1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Nautical Star', 'price' => 7.99, 'quantity' => $sub_cart_quantity+1, 'subtotal' => 7.99*($sub_cart_quantity+1)];
 		        		$request->session()->put('cart_total', $cart_total+7.99);
 		        	}
 		        	else{
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Nautical Star', 'price' => '7.99', 'quantity' => 1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Nautical Star', 'price' => 7.99, 'quantity' => 1, 'subtotal' => 7.99];
 		        		$request->session()->put('cart_total', 7.99);
 		        	} 
 		            break;
 		        case '2':
 		           	if(isset($cart[$_GET['add-to-cart']])){
 		        		$sub_cart_quantity = $cart[$_GET['add-to-cart']]['quantity'];
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Lime', 'price' => '7.99', 'quantity' => $sub_cart_quantity+1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Lime', 'price' => 7.99, 'quantity' => $sub_cart_quantity+1, 'subtotal' => 7.99*($sub_cart_quantity+1)];
 		        		$request->session()->put('cart_total', $cart_total+7.99);
 		        	}
 		        	else {
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Lime', 'price' => '7.99', 'quantity' => 1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Lime', 'price' => 7.99, 'quantity' => 1, 'subtotal' => 7.99];
 		        		$request->session()->put('cart_total', 7.99);
 		        	}
 		            break;
 		        case '3':
 		            if(isset($cart[$_GET['add-to-cart']])){
 		        		$sub_cart_quantity = $cart[$_GET['add-to-cart']]['quantity'];
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Squares', 'price' => '9.99', 'quantity' => $sub_cart_quantity+1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Squares', 'price' => 9.99, 'quantity' => $sub_cart_quantity+1, 'subtotal' => 9.99*($sub_cart_quantity+1)];
 		        		$request->session()->put('cart_total', $cart_total+9.99);
 		        	}
 		        	else {
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Squares', 'price' => '9.99', 'quantity' => 1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Squares', 'price' => 9.99, 'quantity' => 1, 'subtotal' => 9.99];
 		        		$request->session()->put('cart_total', 9.99);
 		        	}
 		            break;
 		        case '4':
 		            if(isset($cart[$_GET['add-to-cart']])){
 		        		$sub_cart_quantity = $cart[$_GET['add-to-cart']]['quantity'];
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Rasta Peace', 'price' => '9.99', 'quantity' => $sub_cart_quantity+1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Rasta Peace', 'price' => 9.99, 'quantity' => $sub_cart_quantity+1, 'subtotal' => 9.99*($sub_cart_quantity+1)];
 		        		$request->session()->put('cart_total', $cart_total+9.99);
 		        	}
 		        	else {
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Rasta Peace', 'price' => '9.99', 'quantity' => 1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Rasta Peace', 'price' => 9.99, 'quantity' => 1, 'subtotal' => 9.99];
 		        		$request->session()->put('cart_total', 9.99);
 		        	}
 		            break;
 		        case '5':
 		            if(isset($cart[$_GET['add-to-cart']])){
 		        		$sub_cart_quantity = $cart[$_GET['add-to-cart']]['quantity'];
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Eyeball', 'price' => '11.99', 'quantity' => $sub_cart_quantity+1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Eyeball', 'price' => 11.99, 'quantity' => $sub_cart_quantity+1, 'subtotal' => 11.99*($sub_cart_quantity+1)];
 		        		$request->session()->put('cart_total', $cart_total+11.99);
 		        	}
 		        	else {
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Eyeball', 'price' => '11.99', 'quantity' => 1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Eyeball', 'price' => 11.99, 'quantity' => 1, 'subtotal' => 11.99];
 		        		$request->session()->put('cart_total', 11.99);
 		        	}
 		            break;
 		        case '6':
 		            if(isset($cart[$_GET['add-to-cart']])){
 		        		$sub_cart_quantity = $cart[$_GET['add-to-cart']]['quantity'];
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Spiral', 'price' => '11.99', 'quantity' => $sub_cart_quantity+1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Spiral', 'price' => 11.99, 'quantity' => $sub_cart_quantity+1, 'subtotal' => 11.99*($sub_cart_quantity+1)];
 		        		$request->session()->put('cart_total', $cart_total+11.99);
 		        	}
 		        	else {
-		        		$cart[$_GET['add-to-cart']] = ['name' => 'Spiral', 'price' => '11.99', 'quantity' => 1];
+		        		$cart[$_GET['add-to-cart']] = ['name' => 'Spiral', 'price' => 11.99, 'quantity' => 1, 'subtotal' => 11.99];
 		        		$request->session()->put('cart_total', 11.99);
 		        	}
 		            break;
